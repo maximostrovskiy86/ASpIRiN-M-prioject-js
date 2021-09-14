1571ccf8ea6005249d6ada66be1c9a76bc20f553
natashagoryanina <natashagoryanina@gmail.com>
2021-09-14 17:31:42 +0300

    genres, date, css cards

--- src/js/components/media.js
+++ src/js/components/media.js
@@ -1,26 +1,47 @@
-import {outputRefs} from "../const/refs";
+import { outputRefs } from "../const/refs";
 import newApiService from "../services/apiSevise";
 import itemMediaTpl from '../../templates/item-media.hbs';
 
-
-
-
 const onLoadPage = async () => {
   const genres = await newApiService.fetchGetGenres();
-  console.log(genres)
 
   const data = await newApiService.fetchGetMediaTrending();
+
+  const genresArr = [...genres.genres];
+
   const result = data.results.map(item => ({
     ...item,
-    genres: genres
-  }))
+    release_date: getDate(item),
+    genre_ids: getGenres([...item.genre_ids])
+  }));
 
-  appendMediaMarkup(data);
+  function getDate(item) {
+    const rDate = new Date(item.release_date);
+    const year = rDate.getFullYear();
+    return year;
+  }
+
+  function getGenres(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < genresArr.length; j++) {
+        if (genresArr[j].id == arr[i]) {
+          newArr.push(genresArr[j].name);
+        }
+      }
+    }
+    return newArr;
+  };
+
+  const newData = { ...data, results: result };
+  console.log(newData);
+
+  appendMediaMarkup(newData);
 }
 
 onLoadPage();
 
-function appendMediaMarkup({results}) {
+function appendMediaMarkup({ results }) {
   return outputRefs.insertAdjacentHTML('beforeend', itemMediaTpl(results));
 }
 
