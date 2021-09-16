3e8ee755036d72ba3dc2c873b5712fb2a498c5ca
IgorSukovsky <sukovsky70@gmail.com>
2021-09-16 17:10:54 +0300

    spinner12

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './sass/main.scss';
-
+import './js/components/spinner';
 import './js/components/header';
 import './js/components/media';
 import './js/components/modal';
 // import './js/components/tui-pagination';
-
--- src/js/const/refs.js
+++ src/js/const/refs.js
@@ -8,3 +8,4 @@ export const headerLink = document.querySelector('.header__container');
 export const outputRefs = document.querySelector('.media-container');
 export const headerRefs = document.querySelector('.header');
 export const formRefs = document.querySelector('#header__form');
+export const spinner = document.querySelector('.mask');
